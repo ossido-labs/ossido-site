2a10ecc1cf0e76cef302026d6373392668ce417c
Chris Schofield <chris.schofield@outlook.com>
2026-08-12 01:24:06 +0100

    simplify lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,4 @@
 export default {
-  '{src,scripts,.storybook}/**/*.{ts,tsx,js,jsx,mjs,cjs}': [
-    'oxlint --fix',
-    'oxfmt',
-  ],
   '*.{ts,mts,cts,js,mjs,cjs}': ['oxlint --fix', 'oxfmt'],
   '*.{ts,mts,cts,cjs}': () => 'tsc -p tsconfig.json --noEmit',
 };
